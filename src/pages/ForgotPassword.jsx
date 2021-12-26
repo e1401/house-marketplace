31511c5997867b672c06c1d5a38ee8f9481bfeab
e1401 <hadrovic1@gmail.com>
2021-12-26 22:01:20 +0100

    92. Forgot password page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,9 +1,59 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast, toastify } from 'react-toastify';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+
 function ForgotPassword() {
+    const [email, setEmail] = useState('');
+
+    const onChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const auth = getAuth();
+            await sendPasswordResetEmail(auth, email);
+            toast.success('Reset e-mail sent!');
+        } catch (error) {
+            toast.error('Reset e-mail could not be sent.');
+        }
+    };
+
     return (
-        <div>
-            Forgot password
+        <div className='pageContainer'>
+            <header>
+                <p className='pageHeader'>Forgot password</p>
+            </header>
+            <main>
+                <form onSubmit={onSubmit}>
+                    <input
+                        type='email'
+                        className='emailInput'
+                        placeholder='E-mail'
+                        id='email'
+                        value={email}
+                        onChange={onChange}
+                    />
+                    <Link className='forgotPasswordLink' to='/sign-in'>
+                        Sign in
+                    </Link>
+                    <div className='signInBar'>
+                        <div className='signInText'>Sent reset link</div>
+                        <button className='signInButton'>
+                            <ArrowRightIcon
+                                fill='#fff'
+                                height='32px'
+                                width='32px'
+                            />
+                        </button>
+                    </div>
+                </form>
+            </main>
         </div>
-    )
+    );
 }
 
-export default ForgotPassword
+export default ForgotPassword;
