ca844183aa9424ad9699d5244dfab2238b509ccb
e1401 <hadrovic1@gmail.com>
2021-12-26 18:28:17 +0100

    89. User logut

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,35 @@
 import {getAuth} from 'firebase/auth'
 import {useState, useEffect} from 'react';
-
+import { useNavigate, Link } from 'react-router-dom';
 
 function Profile() {
     const auth = getAuth();
-    const [user, setUser] = useState(null)
+    const [formData, setFormData] = useState({
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email,
+        
+    })
+
+    const navigate = useNavigate()
+
+    const onLogout = () => {
+        auth.signOut();
+        navigate('/');
+    }
 
 
-    useEffect(() => {
-        setUser(auth.currentUser)
-
-    }, [])
-    return user ? <h1>{user.displayName}</h1> : 'Not logged in'
+    
+    return (
+        <div className="profile">
+        <header className="profileHeader">
+            <p className="pageHeader">My profile</p>
+            <button type="button" className="logOut" onClick={onLogout}>
+                Logout
+            </button>
+        </header>
+    </div>
+    )
+       
     
 }
 
