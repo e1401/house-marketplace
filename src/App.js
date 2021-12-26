81e3ff694bc1eb0fac7dfa533385b676de9986bd
e1401 <hadrovic1@gmail.com>
2021-12-26 12:55:25 +0100

    88. Alerts with React Toastify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css';
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
@@ -22,6 +24,17 @@ function App() {
       {/* navbar here   */}
         <Navbar />
       </Router>
+      <ToastContainer
+          // position="top-left"
+          // autoClose={5000}
+          // hideProgressBar={false}
+          // newestOnTop={false}
+          // closeOnClick
+          // rtl={false}
+          // pauseOnFocusLoss
+          // draggable
+          // pauseOnHover
+          />
       
 
     </>
