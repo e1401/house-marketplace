d248b345279f2e747583420f5adf1995a9b1d8c5
Antonio Hadrović <hadrovic1@gmail.com>
2021-12-29 14:28:12 +0100

    101. Uploading images to firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,10 +1,20 @@
 import { useState, useEffect, useRef } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import {
+    getStorage,
+    ref,
+    uploadBytesResumable,
+    getDownloadURL
+} from 'firebase/storage';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase.config';
 import { useNavigate } from 'react-router-dom';
-import Spinner from '../components/Spinner';
 import { toast } from 'react-toastify';
+import { v4 as uuidv4 } from 'uuid';
+import Spinner from '../components/Spinner';
 
 function CreateListing() {
+    // eslint-disable-next-line
     const [geolocationEnabled, setGeolocationEnabled] = useState(true);
     const [loading, setLoading] = useState(false);
     const [formData, setFormData] = useState({
@@ -42,14 +52,12 @@ function CreateListing() {
     const auth = getAuth();
     const navigate = useNavigate();
     const isMounted = useRef(true);
+
     useEffect(() => {
         if (isMounted) {
             onAuthStateChanged(auth, (user) => {
                 if (user) {
-                    setFormData({
-                        ...formData,
-                        userRef: user.uid
-                    });
+                    setFormData({ ...formData, userRef: user.uid });
                 } else {
                     navigate('/sign-in');
                 }
@@ -59,26 +67,23 @@ function CreateListing() {
         return () => {
             isMounted.current = false;
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isMounted]);
-
-    if (loading) {
-        return <Spinner />;
-    }
 
     const onSubmit = async (e) => {
         e.preventDefault();
+
         setLoading(true);
-        if (discountedPrice > regularPrice) {
+
+        if (discountedPrice >= regularPrice) {
             setLoading(false);
-            toast.error(
-                'Discounted price needs to be lower than regular price.'
-            );
+            toast.error('Discounted price needs to be less than regular price');
             return;
         }
 
         if (images.length > 6) {
             setLoading(false);
-            toast.error('You can upload 6 images max.');
+            toast.error('Max 6 images');
             return;
         }
 
@@ -99,22 +104,92 @@ function CreateListing() {
                 data.status === 'ZERO_RESULTS'
                     ? undefined
                     : data.results[0]?.formatted_address;
+
             if (location === undefined || location.includes('undefined')) {
                 setLoading(false);
-                toast.error('Please enter correct address.');
+                toast.error('Please enter a correct address');
                 return;
             }
         } else {
             geolocation.lat = latitude;
             geolocation.lng = longitude;
-            location = address;
         }
 
+        // Store image in firebase
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage();
+                const fileName = `${auth.currentUser.uid}-${
+                    image.name
+                }-${uuidv4()}`;
+
+                const storageRef = ref(storage, 'images/' + fileName);
+
+                const uploadTask = uploadBytesResumable(storageRef, image);
+
+                uploadTask.on(
+                    'state_changed',
+                    (snapshot) => {
+                        const progress =
+                            (snapshot.bytesTransferred / snapshot.totalBytes) *
+                            100;
+                        console.log('Upload is ' + progress + '% done');
+                        switch (snapshot.state) {
+                            case 'paused':
+                                console.log('Upload is paused');
+                                break;
+                            case 'running':
+                                console.log('Upload is running');
+                                break;
+                            default:
+                                break;
+                        }
+                    },
+                    (error) => {
+                        reject(error);
+                    },
+                    () => {
+                        // Handle successful uploads on complete
+                        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                        getDownloadURL(uploadTask.snapshot.ref).then(
+                            (downloadURL) => {
+                                resolve(downloadURL);
+                            }
+                        );
+                    }
+                );
+            });
+        };
+
+        const imgUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+        ).catch(() => {
+            setLoading(false);
+            toast.error('Images not uploaded');
+            return;
+        });
+
+        const formDataCopy = {
+            ...formData,
+            imgUrls,
+            geolocation,
+            timestamp: serverTimestamp()
+        };
+
+        formDataCopy.location = address;
+        delete formDataCopy.images;
+        delete formDataCopy.address;
+        !formDataCopy.offer && delete formDataCopy.discountedPrice;
+
+        const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
         setLoading(false);
+        toast.success('Listing saved');
+        navigate(`/category/${formDataCopy.type}/${docRef.id}`);
     };
 
     const onMutate = (e) => {
         let boolean = null;
+
         if (e.target.value === 'true') {
             boolean = true;
         }
@@ -122,15 +197,15 @@ function CreateListing() {
             boolean = false;
         }
 
-        ///files
-        if (e.target.filse) {
+        // Files
+        if (e.target.files) {
             setFormData((prevState) => ({
                 ...prevState,
                 images: e.target.files
             }));
         }
 
-        ///text or boolean or numbers
+        // Text/Booleans/Numbers
         if (!e.target.files) {
             setFormData((prevState) => ({
                 ...prevState,
@@ -139,14 +214,19 @@ function CreateListing() {
         }
     };
 
+    if (loading) {
+        return <Spinner />;
+    }
+
     return (
         <div className="profile">
             <header>
                 <p className="pageHeader">Create a Listing</p>
             </header>
+
             <main>
                 <form onSubmit={onSubmit}>
-                    <label className="formLabel">Sell/Rent</label>
+                    <label className="formLabel">Sell / Rent</label>
                     <div className="formButtons">
                         <button
                             type="button"
@@ -175,6 +255,7 @@ function CreateListing() {
                             Rent
                         </button>
                     </div>
+
                     <label className="formLabel">Name</label>
                     <input
                         className="formInputName"
@@ -186,12 +267,13 @@ function CreateListing() {
                         minLength="10"
                         required
                     />
+
                     <div className="formRooms flex">
                         <div>
                             <label className="formLabel">Bedrooms</label>
                             <input
-                                type="number"
                                 className="formInputSmall"
+                                type="number"
                                 id="bedrooms"
                                 value={bedrooms}
                                 onChange={onMutate}
@@ -203,8 +285,8 @@ function CreateListing() {
                         <div>
                             <label className="formLabel">Bathrooms</label>
                             <input
-                                type="number"
                                 className="formInputSmall"
+                                type="number"
                                 id="bathrooms"
                                 value={bathrooms}
                                 onChange={onMutate}
@@ -214,6 +296,7 @@ function CreateListing() {
                             />
                         </div>
                     </div>
+
                     <label className="formLabel">Parking spot</label>
                     <div className="formButtons">
                         <button
@@ -243,6 +326,7 @@ function CreateListing() {
                             No
                         </button>
                     </div>
+
                     <label className="formLabel">Furnished</label>
                     <div className="formButtons">
                         <button
@@ -270,14 +354,15 @@ function CreateListing() {
                             No
                         </button>
                     </div>
+
                     <label className="formLabel">Address</label>
-                    <input
+                    <textarea
+                        className="formInputAddress"
                         type="text"
                         id="address"
                         value={address}
                         onChange={onMutate}
                         required
-                        className="formInputAddress"
                     />
 
                     {!geolocationEnabled && (
@@ -347,10 +432,8 @@ function CreateListing() {
                             max="750000000"
                             required
                         />
-                        {type === 'rent' ? (
+                        {type === 'rent' && (
                             <p className="formPriceText">$ / Month</p>
-                        ) : (
-                            <p className="formPriceText">$</p>
                         )}
                     </div>
 
